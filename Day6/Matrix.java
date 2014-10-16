9fc747e94169f92b86da61d25f8d813b2cb10b1d
Sergio <rurounisergii@hotmail.com>
2014-10-16 13:01:49 +0100

    matrix

--- Day6/Matrix.java
+++ Day6/Matrix.java
@@ -3,6 +3,16 @@ public class Matrix{
 	private int rowLength = 0;
 	private int columnLength = 0;
 
+	public String toString(){
+		String output ="";
+		for (int x=0; x < this.array2d.length; x++){
+			for (int y = 0; y < this.array2d[0].length; y++){
+				output = output + "(" + array2d[x][y] + "),";
+			}
+		}
+		return output;
+	}
+
 	public Matrix(int row, int column){
 		this.array2d = new int[row][column];
 		this.rowLength = row;
@@ -30,16 +40,29 @@ public class Matrix{
 		//count starts at 1 as the number of values in text 67,89,34 will
 		//be the number of commas plus one
 		//decided to use split instead
-		if (temp.length > this.columnLength){
+		if (temp.length > this.array2d[0].length){
 			continue1 = false;
 		}
 		if(continue1 = true){
-			for(int x = 0; x < this.columnLength; x++){
+			for(int x = 0; x < this.array2d[0].length; x++){
 				this.array2d[rowToChange][x] = Integer.parseInt(temp[x]);
 			}
 
 		}
+	}
 
+	public void setColumn(int columnToChange, String text){
+		boolean continue1 = true;
+		String[] temp2 = text.split(",");
+		if(columnToChange > this.array2d[0].length ||
+		temp2.length > this.array2d.length){
+			continue1 = false;
+		}
+		if(continue1 = true){
+			for (int x = 0; x < this.array2d.length; x++){
+				this.array2d[x][columnToChange] = Integer.parseInt(temp2[x]);
+			}
+		}
 	}
 
 
