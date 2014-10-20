0fa5f893980aead22bd62f9089a575e2c84866f9
Sergio <rurounisergii@hotmail.com>
2014-10-20 15:14:31 +0100

    question 3

---
+++ Day7/HospitalManager.java
@@ -0,0 +1,32 @@
+public class HospitalManager{
+	public Patient patientListStart = null;
+
+	public static void main(String[] args){
+		HospitalManager hm = new HospitalManager();
+		hm.launch();
+	}
+
+	public void launch(){
+		Patient firstPatient = new Patient("John", 33, "Tuberculosis");
+		patientListStart = firstPatient;
+		Patient anotherPatient = new Patient("Sam",18,"High Fever");
+		patientListStart.addPatient(anotherPatient);
+		Patient anotherPatient2 = new Patient("Tom",78,"Cancer");
+		patientListStart.addPatient(anotherPatient2);
+		Patient anotherPatient3 = new Patient("Harry",48,"Obesity");
+		patientListStart.addPatient(anotherPatient3);
+		Patient anotherPatient4 = new Patient("Manuel",28,"Aids");
+		patientListStart.addPatient(anotherPatient4);
+		Patient anotherPatient5 = new Patient("Robert",35,"Ebola");
+		patientListStart.addPatient(anotherPatient5);
+		Patient anotherPatient6 = new Patient("Takahito",28,"Kidney stones");
+		patientListStart.addPatient(anotherPatient6);
+		Patient anotherPatient7 = new Patient("Samuel",58,"Diabetes");
+		patientListStart.addPatient(anotherPatient7);
+		Patient anotherPatient8 = new Patient("Chelsea",18,"Broken leg");
+		patientListStart.addPatient(anotherPatient8);
+		Patient anotherPatient9 = new Patient("Katerina",45,"Infection");
+		patientListStart.addPatient(anotherPatient9);
+
+	}
+}
