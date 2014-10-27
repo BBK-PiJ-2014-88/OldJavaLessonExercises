038704706d83e1d657ec39d1801c474a647ec481
Sergio <rurounisergii@hotmail.com>
2014-10-27 15:44:07 +0000

    corrected intset, interfaces cannot have bodies

--- Day9/IntSet.java
+++ Day9/IntSet.java
@@ -3,25 +3,25 @@ public interface IntSet{
 	/**
 	* adds a new int to the set; if it is there already, nothing happens
 	*/
-	public void add(int num){
-	}
+	public void add(int num);
+
 
 	/**
 	* returns true if the number is in the set, false otherwise.
 	*/
-	public boolean contains (int num){
-	}
+	public boolean contains (int num);
+
 
 	/**
 	* returns the same values as the former method, but for every element that is checked
 	* prints its value on the screen
 	*/
-	public boolean containsVerbose(int num){
-	}
+	public boolean containsVerbose(int num);
+
 
 	/**
 	* returns a string with the values of the elements in the set separated by commas
 	*/
-	public String toString(){
-	}
+	public String toString();
+
 }
