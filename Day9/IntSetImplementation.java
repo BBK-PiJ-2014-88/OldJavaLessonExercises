e2d1bddc539307ea8d8ed6833478c445a380734e
Sergio <rurounisergii@hotmail.com>
2014-10-27 15:40:12 +0000

    started the tree implementation of intset

---
+++ Day9/IntSetImplementation.java
@@ -0,0 +1,24 @@
+public class IntSetImplementation implements IntSet{
+	int element = 0;
+	IntSetImplementation right;
+	IntSetImplementation left;
+
+	public IntSetImplementation(int num){
+		this.element = num;
+	}
+
+	public void add(int num){
+	}
+
+
+	public boolean contains (int num){
+	}
+
+	public boolean containsVerbose(int num){
+	}
+
+
+	public String toString(){
+	}
+
+}
