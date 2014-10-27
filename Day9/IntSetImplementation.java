88158762c7de195c487650244b1b448956eb5e23
Sergio <rurounisergii@hotmail.com>
2014-10-27 15:57:10 +0000

    added add, contains and contains verbose method

--- Day9/IntSetImplementation.java
+++ Day9/IntSetImplementation.java
@@ -7,14 +7,71 @@ public class IntSetImplementation implements IntSet{
 		this.element = num;
 	}
 
-	public void add(int num){
+	public void add(int newNum){
+		if(this.contains(newNum) == false){
+			if (newNum > this.element){
+				if (this.right == null){
+					this.right = new IntSetImplementation(newNum);
+				}
+				else{
+					this.right.add(newNum);
+				}
+			}
+			else{
+				if (this.left == null){
+					this.left = new IntSetImplementation(newNum);
+				}
+				else{
+					this.left.add(newNum);
+				}
+			}
+		}
 	}
 
 
 	public boolean contains (int num){
+		if (this.element == num){
+			return true;
+		}
+		else if (num > this.element){
+			if (this.right == null){
+				return false;
+			}
+			else{
+				return this.right.contains(num);
+			}
+		}
+		else{
+			if (this.left == null){
+				return false;
+			}
+			else{
+				return this.left.contains(num);
+			}
+		}
 	}
 
 	public boolean containsVerbose(int num){
+			System.out.println("Checked: " + this.element);
+			if (this.element == num){
+				return true;
+			}
+			else if (num > this.element){
+				if (this.right == null){
+					return false;
+				}
+				else{
+					return this.right.contains(num);
+				}
+			}
+			else{
+				if (this.left == null){
+					return false;
+				}
+				else{
+					return this.left.contains(num);
+				}
+		}
 	}
 
 
