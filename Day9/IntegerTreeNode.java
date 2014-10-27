e63329f0a94284834368db3ff04080f01c3ab81a
Sergio <rurounisergii@hotmail.com>
2014-10-27 14:48:48 +0000

    finished treenode toString method

---
+++ Day9/IntegerTreeNode.java
@@ -0,0 +1,118 @@
+public class IntegerTreeNode{
+	int value;
+	int levels = 0;
+	IntegerTreeNode right;
+	IntegerTreeNode left;
+
+	public IntegerTreeNode(int treeValue){
+		this.value = treeValue;
+	}
+
+	public String toString(){
+		String toPrint = "";
+
+		toPrint = toPrint + "[" + this.value;
+		if (this.left == null && this.right == null){
+			toPrint = toPrint + " L[] R[]]";
+		}
+		if(this.left != null && this.right == null){
+			toPrint = toPrint + " L" + this.left.toString() + "]" + " R[]]";
+	    }
+		if (this.right != null && this.left == null){
+			toPrint = toPrint + " L[] R" + this.right.toString() + "]";
+		}
+		if (this.left != null && this.right != null){
+			toPrint = toPrint + " L" + this.left.toString() + " R" + this.right.toString() + "]";
+		}
+
+		return toPrint;
+	}
+
+
+	public String simplifiedToString(){
+		String toPrint = "";
+
+		toPrint = toPrint + "[" + this.value;
+		if (this.left == null && this.right == null){
+			toPrint = toPrint + "]";
+		}
+		if(this.left != null && this.right == null){
+			toPrint = toPrint + this.left.simplifiedToString() + "]";
+	    }
+		if (this.right != null && this.left == null){
+			toPrint = toPrint + "[" + this.right.simplifiedToString() + "]";
+		}
+		if (this.left != null && this.right != null){
+			toPrint = toPrint + this.left.simplifiedToString() + this.right.simplifiedToString() + "]";
+		}
+
+		return toPrint;
+	}
+
+
+
+	public int depth(){
+		return 1;
+	}
+
+	public int getMax(){
+		int maximum = this.value;
+
+		while(right != null){
+			maximum = right.getMax();
+		}
+		return maximum;
+
+	}
+
+	public int getMin(){
+		int minimum = this.value;
+
+		while(left != null){
+			minimum = left.getMin();
+		}
+		return minimum;
+
+	}
+
+	public void add(int newNumber){
+		if (newNumber > this.value){
+			if (right == null){
+				right = new IntegerTreeNode(newNumber);
+			}
+			else{
+				right.add(newNumber);
+			}
+		}
+		else{
+			if (left == null){
+				left = new IntegerTreeNode(newNumber);
+			}
+			else{
+				left.add(newNumber);
+			}
+		}
+	}
+
+	public boolean contains(int num){
+		if (num == this.value){
+			return true;
+		}
+		else if (num > this.value){
+			if (right == null){
+				return false;
+			}
+			else {
+				return right.contains(num);
+			}
+		}
+		else{
+			if (left == null){
+				return false;
+			}
+			else{
+				return left.contains(num);
+			}
+		}
+	}
+}
---
+++ Day9/IntegerTreeNodeScript.java
@@ -0,0 +1,17 @@
+public class IntegerTreeNodeScript{
+	public static void main(String[] args){
+		IntegerTreeNodeScript treeTester = new IntegerTreeNodeScript();
+		treeTester.launch();
+	}
+	public void launch(){
+			IntegerTreeNode treeTester = new IntegerTreeNode(6);
+			treeTester.add(9);
+			treeTester.add(5);
+			treeTester.add(3);
+			treeTester.add(8);
+			treeTester.add(11);
+			treeTester.add(12);
+			System.out.println(treeTester);
+			System.out.println(treeTester.simplifiedToString());
+	}
+}
